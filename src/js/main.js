587e82643dc41a1b04d4c72c73f8beb8a2d5d95d
kurniawan191008-art <kurniawan191008@gmail.com>
2026-07-26 23:24:29 +0700

    integrasi github api

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { initNavigation } from "./navigation.js";
 import { handleLocationChange } from "./router.js";
 import { initGlow } from "./animation.js";
+import { initGithubData } from "./github.js";
 initGlow();
 initNavigation();
 handleLocationChange();
+initGithubData();
 
--- src/js/pages.js
+++ src/js/pages.js
@@ -7,6 +7,10 @@ export const routes = {
         path: "/about",
         file: "src/pages/about.html"
     },
+    overview: {
+        path: "/overview",
+        file: "src/pages/overview.html"
+    },
     404: {
         path: "/404",
         file: "src/pages/404.html"
