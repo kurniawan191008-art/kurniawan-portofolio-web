9bf8c7db214e79cdd3305c51e08cdc2a4f265174
kurniawan191008-art <kurniawan191008@gmail.com>
2026-07-28 23:09:16 +0700

    desaign overview.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import { initNavigation } from "./navigation.js";
 import { handleLocationChange } from "./router.js";
 import { initGlow } from "./animation.js";
-import { initGithubData } from "./github.js";
+// import { initGithubData } from "./github.js";
 initGlow();
 initNavigation();
 handleLocationChange();
-initGithubData();
+// initGithubData();
 
